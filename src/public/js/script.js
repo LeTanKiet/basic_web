430e378d5be3ac7c49a2d7b1fb36171c54bc86ed
Dat Phan <phanduongquocdat273@gmail.com>
2023-12-28 03:49:04 +0700

    create search bar

---
+++ src/public/js/script.js
@@ -0,0 +1,36 @@
+(function ($) {
+  'use strict';
+
+  var searchPopup = function () {
+    // Open search box
+    $('#navbar').on('click', '.search-button', function (e) {
+      $('.search-popup').toggleClass('is-visible');
+    });
+
+    $('#navbar').on('click', '.btn-close-search', function (e) {
+      $('.search-popup').toggleClass('is-visible');
+    });
+
+    $('.search-popup-trigger').on('click', function (b) {
+      b.preventDefault();
+      $('.search-popup').addClass('is-visible'),
+        setTimeout(function () {
+          $('.search-popup').find('#search-popup').focus();
+        }, 350);
+    }),
+      $('.search-popup').on('click', function (b) {
+        ($(b.target).is('.search-popup-close') ||
+          $(b.target).is('.search-popup-close svg') ||
+          $(b.target).is('.search-popup-close path') ||
+          $(b.target).is('.search-popup')) &&
+          (b.preventDefault(), $(this).removeClass('is-visible'));
+      }),
+      $(document).keyup(function (b) {
+        '27' === b.which && $('.search-popup').removeClass('is-visible');
+      });
+  };
+
+  $(document).ready(function () {
+    searchPopup();
+  }); // End of a document ready
+})(jQuery);

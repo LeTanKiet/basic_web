d1cfbc4378bd4e2f6166762728e60a9f0030f216
Dat Phan <phanduongquocdat273@gmail.com>
2024-01-04 01:42:31 +0700

    rewrite the add-to-cart logic

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -1,20 +1,19 @@
 $(document).ready(function () {
-  //   $('.btn-primary').click(function (event) {
   $('.addToCart').click(function (event) {
     event.preventDefault();
 
     // Get the product details
-    var productCard = $(this).closest('.product-card');
-    var id = productCard.find('a[href^="/products/"]').attr('href').split('/')[2];
-    var name = productCard.find('.title a').text();
-    var price = productCard.find('.price').text().replace('$', '');
-    var image = productCard.find('.image-holder img').attr('src');
+    const productCard = $(this).closest('.product-card');
+    const id = productCard.find('a[href^="/products/"]').attr('href').split('/')[2];
+    const name = productCard.find('.title a').text();
+    const price = productCard.find('.price').text().replace('$', '');
+    const image = productCard.find('.image-holder img').attr('src');
 
     // Create a product object
-    var product = { id: id, name: name, price: price, image: image };
+    const product = { id, name, price, image };
 
     // Get the current cart from local storage
-    var cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
     // Add the product to the cart
     cart.push(product);
@@ -26,19 +25,26 @@ $(document).ready(function () {
     updateCartDisplay();
   });
 
-  // Get the cart from local storage
-  var cart = JSON.parse(localStorage.getItem('cart')) || [];
+  function updateCartDisplay() {
+    // Get the cart from local storage
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-  // Get the offcanvas body
-  var offcanvasBody = $('.offcanvas-body');
+    // Get the offcanvas body
+    const offcanvasBody = $('.offcanvas-body');
 
-  // Clear the offcanvas body
-  offcanvasBody.empty();
+    // Clear the offcanvas body
+    offcanvasBody.empty();
 
-  // Loop through the cart items
-  cart.forEach(function (product) {
+    // Loop through the cart items
+    cart.forEach(function (product) {
+      // Append the cart item HTML to the offcanvas body
+      offcanvasBody.append(createCartItemHtml(product));
+    });
+  }
+
+  function createCartItemHtml(product) {
     // Create the HTML for the cart item
-    var cartItemHtml = `
+    return `
       <div class='card mb-3 shadow-lg' style='max-width: 540px;'>
         <div class='row g-0'>
           <div class='col-md-4'>
@@ -59,48 +65,8 @@ $(document).ready(function () {
         </div>
       </div>
     `;
-
-    // Append the cart item HTML to the offcanvas body
-    offcanvasBody.append(cartItemHtml);
-  });
-
-  function updateCartDisplay() {
-    // Get the cart from local storage
-    var cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-    // Get the offcanvas body
-    var offcanvasBody = $('.offcanvas-body');
-
-    // Clear the offcanvas body
-    offcanvasBody.empty();
-
-    // Loop through the cart items
-    cart.forEach(function (product) {
-      // Create the HTML for the cart item
-      var cartItemHtml = `
-        <div class='card mb-3 shadow-lg' style='max-width: 540px;'>
-          <div class='row g-0'>
-            <div class='col-md-4'>
-              <div class='w-100 overflow-hidden d-flex justify-content-center align-items-center' style='aspect-ratio: 1 / 1;'>
-                <img src='/images/vase.jpg' class='img-fluid rounded-start' alt='image' />
-              </div>
-            </div>
-
-            <div class='col-md-8'>
-              <div class='card-body d-flex align-items-start justify-content-between'>
-                <div>
-                  <h5 class='card-title'>${product.name}</h5>
-                  <h5 class='card-title' style='color:#85BB65;'>${product.price}</h5>
-                </div>
-                <i class='bi bi-trash fs-5'></i>
-              </div>
-            </div>
-          </div>
-        </div>
-    `;
-
-      // Append the cart item HTML to the offcanvas body
-      offcanvasBody.append(cartItemHtml);
-    });
   }
+
+  // Initial cart display update
+  updateCartDisplay();
 });

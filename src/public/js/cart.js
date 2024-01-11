a9133eefb94e9e7611bac4dac7ca2a2bea80cfdd
Dat Phan <phanduongquocdat273@gmail.com>
2024-01-11 15:23:10 +0700

    fix cart UI

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
   function createCartItemHtml(product) {
     // Create the HTML for the cart item
     return `
-      <div class='cart-item card mb-3 shadow-sm'>
+      <div class='cart-item card my-1 shadow-sm'>
         <div class='row g-0'>
           <div class='col-md-4'>
             <div class='w-100 overflow-hidden d-flex justify-content-center align-items-center' style='aspect-ratio: 1 / 1;'>

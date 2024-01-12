c7dc2d78dac1fb3a6014c0572d34c4539d2166cf
Dat Phan <phanduongquocdat273@gmail.com>
2024-01-12 15:23:44 +0700

    product page pagination

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -6,13 +6,42 @@ class ProductController {
     const { userId } = req.context;
     const user = await db.oneOrNone('select * from "users" where id = $1', userId);
 
-    // Query the database for all products
-    const products = await db.any('select * from "products"');
-
     // TODO: Query all categories from the database instead of using the constants
     // TODO: Query all colors from the database instead of using the constants
     // TODO: Query all materials from the database instead of using the constants
     // TODO: Query all countryOfOrigin from the database instead of using the constants
+
+    // Pagination
+
+    // Get the current page number from the query parameters, or default to 1
+    let currentPage = parseInt(req.query.page) || 1;
+    // Ensure currentPage is a number, if it's not, set it to 1
+    if (typeof currentPage !== 'number' || currentPage <= 0) {
+      currentPage = 1;
+    }
+
+    // Define the number of products per page
+    const productsPerPage = 4;
+
+    // Query the database for the total number of products
+    const totalProducts = await db.one('select count(*) from "products"', [], (data) => Number(data.count));
+
+    // Calculate the total number of pages
+    const totalPages = Math.ceil(totalProducts / productsPerPage);
+
+    // If currentPage is greater than totalPages, set it to totalPages
+    if (currentPage > totalPages) {
+      currentPage = totalPages;
+    }
+
+    // Calculate the offset for the database query
+    const offset = (currentPage - 1) * productsPerPage;
+
+    // Query the database for the products on the current page
+    const products = await db.any('select * from "products" order by id limit $1 offset $2', [productsPerPage, offset]);
+
+    // Create an array representing all pages: [1, 2, ..., totalPages]
+    const pageNumbersArray = Array.from({ length: totalPages }, (_, i) => i + 1);
 
     return res.render('products', {
       user,
@@ -21,6 +50,9 @@ class ProductController {
       colors,
       materials,
       countryOfOrigin,
+      currentPage,
+      totalPages,
+      pageNumbersArray,
     });
   }
 
--- src/hooks/useHandlebars.js
+++ src/hooks/useHandlebars.js
@@ -1,7 +1,29 @@
 import { engine } from 'express-handlebars';
 
 export function useHandlebars(app) {
-  app.engine('hbs', engine({ extname: 'hbs' }));
+  app.engine(
+    'hbs',
+    engine({
+      extname: 'hbs',
+      helpers: {
+        eq: function (v1, v2) {
+          return v1 === v2;
+        },
+        increment: function (value) {
+          return parseInt(value) + 1;
+        },
+        decrement: function (value) {
+          return parseInt(value) - 1;
+        },
+        isFirstPage: function (value) {
+          return value === 1;
+        },
+        isLastPage: function (value, totalPages) {
+          return value === totalPages;
+        },
+      },
+    }),
+  );
   app.set('view engine', 'hbs');
   app.set('views', './src/views/app');
 }
